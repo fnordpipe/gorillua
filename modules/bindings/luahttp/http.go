09b2c181c97a8ff9cd4763d8a9792ec032d51e98
crito <crito@fnordpipe.org>
2019-04-13 15:16:05 +0200

    fix wrong references in http request multiplexing

--- modules/bindings/luahttp/http.go
+++ modules/bindings/luahttp/http.go
@@ -24,7 +24,7 @@ func Loader(L *lua.LState) int {
   return 1
 }
 
-func handleRequest(L *lua.LState, ctx *RouterInfo, w http.ResponseWriter, r *http.Request) {
+func handleRequest(L *lua.LState, ctx RouterInfo, w http.ResponseWriter, r *http.Request) {
   var _w = map[string]lua.LGFunction{
     "addHeader": func(L *lua.LState) int {
       key := L.CheckString(1)
@@ -72,7 +72,7 @@ func serve(L *lua.LState) int {
   lrouter := L.CheckTable(2)
   lstatic := L.CheckAny(3)
   router := mux.NewRouter()
-  var r []RouterInfo
+  var routes []RouterInfo
 
   lrouter.ForEach(func(k, v lua.LValue) {
     var route RouterInfo
@@ -83,14 +83,15 @@ func serve(L *lua.LState) int {
           if k.String() == "context" { route.Context = v.String() }
           if k.String() == "callback" { route.Callback = v }
         })
-        r = append(r, route)
+        routes = append(routes, route)
     }
   })
 
-  for _, v := range r {
-    router.HandleFunc(v.Context, func(w http.ResponseWriter, r *http.Request) {
-      handleRequest(L, &v, w, r)
-    }).Methods(v.Method)
+  for k, _ := range routes {
+    j := k
+    router.HandleFunc(routes[j].Context, func(w http.ResponseWriter, r *http.Request) {
+      handleRequest(L, routes[j], w, r)
+    }).Methods(routes[j].Method)
   }
 
   switch lv := lstatic.(type) {
